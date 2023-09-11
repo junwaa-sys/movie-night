dd10fc80312d7cf75eb55c8609c264e4c7e27082
joon jeong <juny.jeong@gmail.com>
2023-09-12 11:17:26 +1200

    added loading status and display logout button when logged in

--- client/components/TopNav.tsx
+++ client/components/TopNav.tsx
@@ -2,18 +2,26 @@ import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 
 function TopNav() {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0()
+  const { user, isLoading, loginWithRedirect, isAuthenticated, logout } =
+    useAuth0()
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
 
   return (
     <div className="topnav">
       <div className="topnav-container">
-        <a href="#Home">HOME</a>
-        {isAuthenticated ? <button className="button" onClick={() => loginWithRedirect()}>
-          LOGIN
-        </button> :<button className="button" onClick={() => logout()}>
-          LOG OUT
-        </button>}
-        
+        <div>You are logged in as {user?.name}</div>
+        {!isAuthenticated ? (
+          <button className="button" onClick={() => loginWithRedirect()}>
+            LOGIN
+          </button>
+        ) : (
+          <button className="button" onClick={() => logout()}>
+            LOG OUT
+          </button>
+        )}
       </div>
     </div>
   )
