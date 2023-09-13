0bd6ea9d4464a48fe191f66ac7a9dcefb30a8867
joon jeong <juny.jeong@gmail.com>
2023-09-14 11:37:14 +1200

    add column to movies table

--- server/db/migrations/20230831064825_movies.js
+++ server/db/migrations/20230831064825_movies.js
@@ -6,6 +6,12 @@ exports.up = function (knex) {
   return knex.schema.createTable('movies', (table) => {
     table.string('id')
     table.string('title')
+    table.string('director')
+    table.string('language')
+    table.string('genre')
+    table.string('plot')
+    table.string('poster_url')
+    table.string('actors')
     table.string('released_date')
   })
 }
