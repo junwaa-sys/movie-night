8e4bd0eed2b5ba8c9aaf7ee5a152fa6d7b7454ed
joon jeong <juny.jeong@gmail.com>
2023-09-18 11:20:18 +1200

    trying to implent mentine style

--- client/actions/index.ts
+++ client/actions/index.ts
@@ -1,21 +1,21 @@
-import { getFruits } from '../apis/fruits'
-import type { ThunkAction } from '../store'
+// import { getFruits } from '../apis/fruits'
+// import type { ThunkAction } from '../store'
 
-export type Action = { type: typeof SET_FRUITS; payload: string[] }
+// export type Action = { type: typeof SET_FRUITS; payload: string[] }
 
-export const SET_FRUITS = 'SET_FRUITS'
+// export const SET_FRUITS = 'SET_FRUITS'
 
-export function setFruits(fruits: string[]): Action {
-  return {
-    type: SET_FRUITS,
-    payload: fruits,
-  }
-}
+// export function setFruits(fruits: string[]): Action {
+//   return {
+//     type: SET_FRUITS,
+//     payload: fruits,
+//   }
+// }
 
-export function fetchFruits(): ThunkAction {
-  return (dispatch) => {
-    return getFruits().then((fruits) => {
-      dispatch(setFruits(fruits))
-    })
-  }
-}
+// export function fetchFruits(): ThunkAction {
+//   return (dispatch) => {
+//     return getFruits().then((fruits) => {
+//       dispatch(setFruits(fruits))
+//     })
+//   }
+// }
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,11 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import TopNav from './TopNav'
 import Main from './Main'
 import MoiveList from './MoiveList'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { user } = useAuth0()
+
   return (
     <>
-      <TopNav />
+      <TopNav user={{ name: user?.name, image: user?.picture }} tabs={[]} />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/movie" element={<Main />} />
