03efcd648c333c99b3e0353b9373cb65ddbd3b28
joon jeong <juny.jeong@gmail.com>
2023-09-18 13:12:33 +1200

    update auth0 setting

--- client/index.tsx
+++ client/index.tsx
@@ -9,7 +9,7 @@ import App from './components/App'
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
-      domain="dev-q2tnfzhtn06lumms.us.auth0.com"
+      domain="https://tohora-2023-joon.au.auth0.com"
       clientId="NNap7rNfgxguGPLT2obq6M3J46Z7E7JS"
       redirectUri={`${window.location.origin}`}
       audience="https://moive-night/api"
