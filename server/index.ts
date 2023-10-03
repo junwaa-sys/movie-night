b575d13ccb032d29298b1b816ffa23f6f7f1383e
joon jeong <juny.jeong@gmail.com>
2023-10-04 02:14:42 +1300

    change local host address

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import server from './server'
 
 const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || '0.0.0.0'
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
