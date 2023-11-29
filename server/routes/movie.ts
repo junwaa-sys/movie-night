d6181eecb8e3509e2ff3266cb4b911f12ab6c750
joon jeong <juny.jeong@gmail.com>
2023-11-29 19:50:26 +1300

    update random movie api

--- server/routes/movie.ts
+++ server/routes/movie.ts
@@ -7,24 +7,22 @@ const router = express.Router()
 const axios = require('axios')
 
 const options = {
-  params: {
-    list: 'most_pop_movies',
-  },
+  method: 'GET',
+  url: 'https://moviesdatabase.p.rapidapi.com/titles/random',
   headers: {
     'X-RapidAPI-Key': process.env.AXIOS_MOVIE_API_KEY,
     'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com',
   },
 }
 
-router.get('/get/random', (req, res) => {
-  axios
-    .get('https://moviesdatabase.p.rapidapi.com/titles/random', options)
-    .then((response: any) => {
-      res.json(response.data)
-    })
-    .catch((error: any) => {
-      console.log(error)
-    })
+router.get('/get/random', async (req, res) => {
+  try {
+    const response = await axios.request(options)
+    console.log(response)
+    res.json(response.body)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.get('/get-details/:id', (req, res) => {
